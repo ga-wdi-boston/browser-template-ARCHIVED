c2347cd17e5bb50a7ce13e13e39c1d3da007eea2
Jeffrey Horn <jeffh@generalassemb.ly>
2015-07-01 04:05:51 -0400

    Add html and js templates for quizzes

---
+++ quiz.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// quiz questions should match objectives
+// keep it simple
+// be very specific in your directions
+// 4 questions = about 20 minutes for students to complete
+// replace the example questions with your own
+
+
+// Question 1 //////////////////////////////////////////////////////////////////
+
+var pets = ["fido", "sparky", "fluffy"];
+
+// Write code to change the last entry in this array from "fluffy" to "spike".
+// Do not modify the code above.
+
+// YOUR CODE HERE
+pets[2] = "spike";
+
+////////////////////////////////////////////////////////////////////////////////
+
+// DO NOT CHANGE
+module.exports = {
+  answer1: pets
+};
