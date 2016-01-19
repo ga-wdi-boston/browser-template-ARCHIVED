1cc4a683cf8e769d0df62c85089c27055ae03b04
Jeffrey Horn <jeffh@generalassemb.ly>
2016-01-19 10:20:22 -0500

    Add automatic manifest for specs

---
+++ spec/_all.js
@@ -0,0 +1,4 @@
+// Load all specs so webpack can find them. Think of this as an automatic
+// manifest for bundling specs.
+var req = require.context('./', true, /spec\.js$/);
+req.keys().forEach(req);
