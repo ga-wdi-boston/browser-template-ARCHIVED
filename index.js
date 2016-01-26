dc4dcbc3c03be7b6ef402467ae6e91962992d742
Jeffrey Horn <jeffh@generalassemb.ly>
2016-01-26 16:20:57 -0500

    Actually attach $ and jQuery to window
    Previously, I had concluded that was the job of ProvidePlugin in webpack.
    Instead, ProvidePlugin makes these things available *within* modules. For
    example:
    
    ```js
    new webpack.ProvidePlugin({
      "window.jQuery": "jquery"
    })
    ```
    
    Now, anything bundled that you don't own (say bootstrap.js?) that references
    the global jQuery will find it available. It isn't attached to the actual
    window. It's "webpack's window".

--- index.js
+++ index.js
@@ -6,3 +6,7 @@
 // load manifests
 require('./assets/scripts/index.js');
 require('./assets/styles/index.scss');
+
+// attach jQuery globally
+require("expose?$!jquery");
+require("expose?jQuery!jquery");
