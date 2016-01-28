f531f752d6bf3793b481084d86ddb9fd4879c2a5
Jeffrey Horn <jeffh@generalassemb.ly>
2016-01-28 08:23:44 -0500

    Fix mixed quotes

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ require('./assets/scripts/index.js');
 require('./assets/styles/index.scss');
 
 // attach jQuery globally
-require("expose?$!jquery");
-require("expose?jQuery!jquery");
+require('expose?$!jquery');
+require('expose?jQuery!jquery');
