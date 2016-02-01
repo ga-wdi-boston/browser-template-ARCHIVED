cb8d91c13ef65efe7f242cf11f36e628c551b85a
Jeffrey Horn <jeffh@generalassemb.ly>
2016-01-31 19:42:36 -0500

    Fix linter warnings

--- spec/example.spec.js
+++ spec/example.spec.js
@@ -2,8 +2,8 @@
 
 var example = require('../assets/scripts/example');
 
-describe('Example', function() {
-  it('is true', function() {
+describe('Example', function () {
+  it('is true', function () {
     expect(example).toBe(true);
   });
 });
