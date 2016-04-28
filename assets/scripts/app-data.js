d4617e20a4ad560afa959892e3a56765b02b9a70
V4Vince <vince.guoo@gmail.com>
2016-04-28 16:47:16 -0400

    sets up file skeleton

---
+++ assets/scripts/app-data.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const app = {
+  api: ''
+};
+module.exports = app;
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,10 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+const authEvents = require('./auth/events.js');
+
+// On document ready
+$(() => {
+  authEvents.addHandlers();
+});
