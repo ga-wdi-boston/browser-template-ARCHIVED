c47de3e81e99eb9b24cd6fddeffe2ce94c4e6650
Antony Donovan <antony.donovan@generalassemb.ly>
2016-09-17 17:43:22 -0400

    Use GA as the basis for the client port
    Make development ports consistent among front end templates

--- grunt/webpack-dev-server.js
+++ grunt/webpack-dev-server.js
@@ -5,12 +5,15 @@ let clone = require('clone');
 // clone the webpack config to separate configuration of webpack and dev server
 let webpackConfig = clone(require('./webpack').options);
 
+//port for webpack-dev-server
+const port = +('GA'.split('').reduce((p, c)=> p + c.charCodeAt(), ''));
+
 // enable live reload without a script tag
-webpackConfig.entry.vendor.unshift('webpack-dev-server/client?http://localhost:4200');
+webpackConfig.entry.vendor.unshift(`webpack-dev-server/client?http://localhost:${port}`);
 
 module.exports = {
   options: {
-    port: 4200,
+    port,
     webpack: webpackConfig,
   },
 
