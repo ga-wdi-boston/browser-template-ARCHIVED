b419990b939015cb557b9b5e1902becb0e45dee2
racheltstevens <racheltstevens@gmail.com>
2016-10-28 09:14:07 -0400

    Close #42 Remove getFormFields from index.js
    Get formFields is no longer exposed globally.

--- index.js
+++ index.js
@@ -13,7 +13,3 @@ require('./assets/styles/index.scss');
 // attach jQuery globally
 require('expose?$!jquery');
 require('expose?jQuery!jquery');
-
-// attach getFormFields globally
-
-require('expose?getFormFields!./lib/get-form-fields.js');
