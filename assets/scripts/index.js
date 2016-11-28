97dfc00b880b3724e22614e3017ce4c63b762540
Antony Donovan <antony.donovan@generalassemb.ly>
2016-11-28 10:17:35 -0500

    Don't use `var`
    Even in commented out examples
    Close #63

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example');
 
 // use require without a reference to ensure a file is bundled
 require('./example');
