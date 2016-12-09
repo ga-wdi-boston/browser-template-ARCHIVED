d0a0b169f74b78aaaf6af24a4579f92c15b1412a
jbuzzanco <jcbuzzanco@gmail.com>
2016-12-09 14:11:28 -0500

    heroku, handlebars update select all input
    heroku, handlebars update select all input
    for update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = {
- host: 'https://radiant-retreat-75178.herokuapp.com'
+  host: 'http://localhost:4741'
+
+ // host: 'https://radiant-retreat-75178.herokuapp.com'
 };
