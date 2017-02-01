71b3ab6fa33e4462bfcbc4824b2f98ddf64a5b8f
Antony Donovan <antony.donovan@generalassemb.ly>
2017-02-01 12:41:29 -0500

    Refactor manifest; move config.js to app root
    Load script, template, and style indices from template manifest.
    Move config to clarify its purpose.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,15 +1,5 @@
 'use strict'
 
-const setAPIOrigin = require('../../lib/set-api-origin')
-const config = require('./config')
-const main = require('../templates/index.hbs')
-
-$(() => {
-  $('title').text('For example')
-  $('body').append(main)
-  setAPIOrigin(location, config)
-})
-
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict'
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
-
 // load manifests
+
+const setAPIOrigin = require('./lib/set-api-origin');
+const config = require('./app/config');
+const main = require('./app/templates/index.hbs');
+
+$(() => {
+  $('title').text('For example');
+  $('body').append(main);
+  setAPIOrigin(location, config);
+});
+
 // scripts
 require('./app/scripts/index.js')
 
