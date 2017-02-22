c7cbcc95e38d303d19c4be1840854b877f5676f4
Antony Donovan <antony.donovan@generalassemb.ly>
2017-02-22 13:31:51 -0500

    Add ids to example usage
    For click handlers, etc.

--- app/scripts/example.js
+++ app/scripts/example.js
@@ -4,14 +4,23 @@ const bobTemplate = require('../templates/lists/books.hbs')
 const section = 'Dystopian Novels'
 
 const books = [
-  { title: 'Animal Farm', author: 'George Orwell' },
-  { title: '1984', author: 'George Orwell' },
-  { title: 'Anthem', author: 'Ayn Rand' }
+  { id: 3, title: 'Animal Farm', author: 'George Orwell' },
+  { id: 2, title: '1984', author: 'George Orwell' },
+  { id: 1, title: 'Anthem', author: 'Ayn Rand' }
 ]
 
 $(() => {
   const html = bobTemplate({ books, section })
   $('#view').html(html)
+  $('#book-list').on('click', '.retcon', function (e) {
+    // true for button.retcon click, not icon click
+    if (e.target === this) {
+      console.log('yay?')
+    }
+
+    const id = $(this).parent().data('book-id')
+    console.log('book.id', id)
+  })
 })
 
 module.exports = true
